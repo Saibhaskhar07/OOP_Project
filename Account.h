00cbbda0a03e1e714dd906451edb182007d227c4
“Saibhaskhar <saibhaskharr@gmail.com>
2024-05-08 19:58:41 +0930

    V 1.1

---
+++ Account.h
@@ -0,0 +1,18 @@
+#include <iostream> 
+#include <string>
+using namespace std ;
+
+
+class Account{
+    // Attributes required for account creation
+    protected :
+        string Name ;
+        string Email ;
+        int Age ;
+        long int Acc_No ;
+        string Transactions ;
+
+    public : 
+    
+
+} ;
