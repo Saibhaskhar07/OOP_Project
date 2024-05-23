c75ac7ea8be530d6837d704b5a239454c948f13a
JeganBhaskaran <161258998+JeganBhaskaran@users.noreply.github.com>
2024-05-23 22:51:54 +0930

    Update PersonalLoan.h

--- PersonalLoan.h
+++ PersonalLoan.h
@@ -4,16 +4,16 @@
 #include "Loan.h"
 #include <string>
 
-namespace bankeasy {
+namespace bankeasy { // Define namespace bankeasy
 
-class PersonalLoan : public Loan {
+class PersonalLoan : public Loan { // Define Personal Loan class from Loan
 public:
-    PersonalLoan(double amount, int tenure, const std::string& purpose);
+    PersonalLoan(double amount, int tenure, const std::string& purpose); // Get Loan parameters
     std::string getLoanType() const override;
     std::string getPurpose() const;
     void changeInterestRate(double newRate) override;
-    void displayRequiredDocuments() const override;
-    void calculateEMI() const override;
+    void displayRequiredDocuments() const override; // Display loan info
+    void calculateEMI() const override; // Calculate EMI
 
 private:
     std::string purpose;
