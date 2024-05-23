c494dcae096c0433a3b5a880edcb253f886ebd74
JeganBhaskaran <161258998+JeganBhaskaran@users.noreply.github.com>
2024-05-23 22:41:32 +0930

    Update PersonalLoan.cpp

--- PersonalLoan.cpp
+++ PersonalLoan.cpp
@@ -1,15 +1,15 @@
 #include "PersonalLoan.h"
 #include <iostream>
 
-namespace bankeasy {
+namespace bankeasy { // Define namespace bankeasy
 
 PersonalLoan::PersonalLoan(double amount, int tenure, const std::string& purpose)
-    : Loan(amount, tenure), purpose(purpose) {
+    : Loan(amount, tenure), purpose(purpose) { // Constructor initializes loan information
     interestRate = 7.83; // Specific interest rate for Personal Loans
 }
 
 std::string PersonalLoan::getLoanType() const {
-    return "Personal Loan";
+    return "Personal Loan"; // Return loan type
 }
 
 std::string PersonalLoan::getPurpose() const {
@@ -24,6 +24,7 @@ void PersonalLoan::displayRequiredDocuments() const {
     std::cout << "- Purpose\n- Proof of Identity\n- Proof of Residence\n- ITR for 2 years\n";
 }
 
+// Calculate and display EMI
 void PersonalLoan::calculateEMI() const {
     double emi = (amount * interestRate / 100) / tenure;
     std::cout << "EMI for the loan is: $" << emi << "\n";
